728a93c472f9429661d2999bf4aec96c7c682e06
FishHawk <whang177@gmail.com>
2023-03-22 18:32:04 +0800

    add main layout to extra files page

--- src-vue/src/views/Extra.vue
+++ src-vue/src/views/Extra.vue
@@ -1,24 +1,32 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
-
 const iframe = ref();
+
+var lastHeight = 0;
 onMounted(() => {
-  // $nextTick(() => {
-  window.addEventListener('load', () => {
-    console.log(iframe.value);
-    // frameHeight = $refs.iframe.contentWindow.document.body.scrollHeight + 'px';
-  });
-  // })
+  window.setInterval(() => {
+    if (iframe.value) {
+      const height =
+        iframe.value.contentWindow.document.documentElement.scrollHeight;
+      if (lastHeight !== height) {
+        lastHeight = height;
+        iframe.value.height =
+          iframe.value.contentWindow.document.documentElement.scrollHeight;
+      }
+    }
+  }, 200);
 });
 </script>
+
 <template>
   <MainLayout>
     <iframe
       ref="iframe"
-      src="https://books.fishhawk.top/files-extra/"
+      src="/files-extra"
+      scrolling="no"
       title="description"
-      style="width: 100%; min-height: calc(100vh - 105px)"
       frameBorder="0"
+      width="100%"
     />
   </MainLayout>
 </template>
