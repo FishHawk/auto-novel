4cb172bded611a1df906a56c6d5046ad70ade042
FishHawk <whang177@gmail.com>
2023-04-09 18:22:45 +0800

    clean baidu translator

--- src-vue/src/data/translator/baidu-web.ts
+++ src-vue/src/data/translator/baidu-web.ts
@@ -2,60 +2,62 @@ import ky from 'ky';
 import { BaiduBaseTranslator } from './baidu';
 import { Glossary } from './base';
 
-function a(r: any, o: any) {
-  for (var t = 0; t < o.length - 2; t += 3) {
-    var a = o.charAt(t + 2);
-    (a = a >= 'a' ? a.charCodeAt(0) - 87 : Number(a)),
-      (a = '+' === o.charAt(t + 1) ? r >>> a : r << a),
-      (r = '+' === o.charAt(t) ? (r + a) & 4294967295 : r ^ a);
+function a(r: number, o: string) {
+  for (let t = 0; t < o.length - 2; t += 3) {
+    const a = o.charAt(t + 2);
+    let aa = a >= 'a' ? a.charCodeAt(0) - 87 : Number(a);
+    aa = '+' === o.charAt(t + 1) ? r >>> aa : r << aa;
+    r = '+' === o.charAt(t) ? (r + aa) & 4294967295 : r ^ aa;
   }
   return r;
 }
-var C: any = null;
-var token = function (r: any, _gtk: any) {
-  var o = r.length;
-  o > 30 &&
-    (r =
+var token = function (r: string, gtk: string) {
+  if (r.length > 30) {
+    r =
       '' +
-      r.substr(0, 10) +
-      r.substr(Math.floor(o / 2) - 5, 10) +
-      r.substring(r.length, r.length - 10));
-  var t: any = void 0,
-    t = null !== C ? C : (C = _gtk || '') || '';
-  for (
-    var e = t.split('.'),
-      h = Number(e[0]) || 0,
-      i = Number(e[1]) || 0,
-      d = [],
-      f = 0,
-      g = 0;
-    g < r.length;
-    g++
-  ) {
-    var m = r.charCodeAt(g);
-    128 > m
-      ? (d[f++] = m)
-      : (2048 > m
-          ? (d[f++] = (m >> 6) | 192)
-          : (55296 === (64512 & m) &&
-            g + 1 < r.length &&
-            56320 === (64512 & r.charCodeAt(g + 1))
-              ? ((m = 65536 + ((1023 & m) << 10) + (1023 & r.charCodeAt(++g))),
-                (d[f++] = (m >> 18) | 240),
-                (d[f++] = ((m >> 12) & 63) | 128))
-              : (d[f++] = (m >> 12) | 224),
-            (d[f++] = ((m >> 6) & 63) | 128)),
-        (d[f++] = (63 & m) | 128));
+      r.substring(0, 10) +
+      r.substring(Math.floor(r.length / 2) - 5, 10) +
+      r.substring(r.length, r.length - 10);
   }
-  for (var S = h, u = '+-a^+6', l = '+-3^+b+-f', s = 0; s < d.length; s++)
-    (S += d[s]), (S = a(S, u));
-  return (
-    (S = a(S, l)),
-    (S ^= i),
-    0 > S && (S = (2147483647 & S) + 2147483648),
-    (S %= 1e6),
-    S.toString() + '.' + (S ^ h)
-  );
+
+  const gtkList = gtk.split('.');
+  const gtk1 = Number(gtkList[0]) || 0;
+  const gtk2 = Number(gtkList[1]) || 0;
+
+  let d = [];
+  let j = 0;
+  for (let i = 0; i < r.length; i++) {
+    var m = r.charCodeAt(i);
+    if (128 > m) {
+      d[j++] = m;
+    } else if (2048 > m) {
+      d[j++] = (m >> 6) | 192;
+    } else {
+      if (
+        55296 === (64512 & m) &&
+        i + 1 < r.length &&
+        56320 === (64512 & r.charCodeAt(i + 1))
+      ) {
+        m = 65536 + ((1023 & m) << 10) + (1023 & r.charCodeAt(++i));
+        d[j++] = (m >> 18) | 240;
+        d[j++] = ((m >> 12) & 63) | 128;
+      } else {
+        d[j++] = (m >> 12) | 224;
+        d[j++] = ((m >> 6) & 63) | 128;
+      }
+      d[j++] = (63 & m) | 128;
+    }
+  }
+
+  let S = gtk1;
+  for (let s = 0; s < d.length; s++) {
+    S = a(S + d[s], '+-a^+6');
+  }
+  S = a(S, '+-3^+b+-f');
+  S ^= gtk2;
+  0 > S && (S = (2147483647 & S) + 2147483648);
+  S %= 1e6;
+  return S.toString() + '.' + (S ^ gtk1);
 };
 
 export class BaiduWebTranslator extends BaiduBaseTranslator {
