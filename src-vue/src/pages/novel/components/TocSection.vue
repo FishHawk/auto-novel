b790da589e877e86f1c7a605389a6187c5a11b57
FishHawk <whang177@gmail.com>
2023-06-13 00:20:08 +0800

    add toc sort button

--- src-vue/src/pages/novel/components/TocSection.vue
+++ src-vue/src/pages/novel/components/TocSection.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { SortFilled } from '@vicons/material';
+import { computed, ref } from 'vue';
 import { useWindowSize } from '@vueuse/core';
 
 import { WebNovelTocItemDto } from '@/data/api/api_web_novel';
@@ -7,17 +8,32 @@ import { WebNovelTocItemDto } from '@/data/api/api_web_novel';
 const { width } = useWindowSize();
 const isDesktop = computed(() => width.value > 600);
 
-defineProps<{
+const props = defineProps<{
   providerId: string;
   novelId: string;
   toc: WebNovelTocItemDto[];
 }>();
+
+const isReverse = ref(false);
+const reverseToc = computed(() => props.toc.slice().reverse());
 </script>
 
 <template>
-  <n-h2 prefix="bar">目录</n-h2>
+  <n-space align="baseline" justify="space-between" style="width: 100">
+    <n-h2 prefix="bar">目录</n-h2>
+    <n-button @click="isReverse = !isReverse">
+      <template #icon>
+        <n-icon> <SortFilled /> </n-icon>
+      </template>
+      {{ isReverse ? '倒序' : '正序' }}
+    </n-button>
+  </n-space>
+
   <n-list>
-    <n-list-item v-for="tocItem in toc" style="padding: 0px">
+    <n-list-item
+      v-for="tocItem in isReverse ? reverseToc : toc"
+      style="padding: 0px"
+    >
       <n-a
         v-if="tocItem.chapterId"
         :href="`/novel/${providerId}/${novelId}/${tocItem.chapterId}`"
