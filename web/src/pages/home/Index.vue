f00cd0459028b1703774e85bfb57b66a14536d67
FishHawk <whang177@gmail.com>
2024-01-20 19:39:51 +0800

    web: update home page

--- web/src/pages/home/Index.vue
+++ web/src/pages/home/Index.vue
@@ -11,12 +11,9 @@ import {
 } from '@/data/api/api_wenku_novel';
 import { Ok, ResultState } from '@/data/result';
 import { useUserDataStore } from '@/data/stores/user_data';
-import { useIsDesktop } from '@/data/util';
 import { parseUrl } from '@/data/util_web';
 import bannerUrl from '@/images/banner.webp';
-import qqUrl from '@/images/qq.png';
 
-const isDesktop = useIsDesktop(900);
 const userData = useUserDataStore();
 const router = useRouter();
 const vars = useThemeVars();
@@ -85,17 +82,29 @@ const loadWenku = async () => {
 };
 loadWenku();
 
-const showLinkExampleModal = ref(false);
+const showHowToUseModal = ref(false);
 const linkExample = [
   ['Kakuyomu', 'https://kakuyomu.jp/works/16817139555217983105'],
-  ['成为小说家吧', 'https://ncode.syosetu.com/n0833hi'],
+  [
+    '成为小说家吧',
+    'https://ncode.syosetu.com/n0833hi <br /> https://novel18.syosetu.com/n3192gh',
+  ],
   ['Novelup', 'https://novelup.plus/story/206612087'],
   ['Hameln', 'https://syosetu.org/novel/297874/'],
-  ['Pixiv系列', 'https://www.pixiv.net/novel/series/9406879'],
-  ['Pixiv短篇', 'https://www.pixiv.net/novel/show.php?id=18304868'],
+  [
+    'Pixiv系列/短篇',
+    'https://www.pixiv.net/novel/series/9406879 <br/> https://www.pixiv.net/novel/show.php?id=18304868',
+  ],
   ['Alphapolis', 'https://www.alphapolis.co.jp/novel/638978238/525733370'],
   ['Novelism', 'https://novelism.jp/novel/2m0xulekSsCxfixwam8d7g'],
 ];
+
+const showQQModal = ref(false);
+const qqLink =
+  'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=Qa0SOMBYZoJZ4vuykz3MbPS0zbpeN0pW&authKey=q75E7fr5CIBSDhqX%2F4kuC%2B0mcPiDvj%2FSDfP%2FGZ8Rl8kDn6Z3M6XPSZ91yt4ZWonq&noverify=0&group_code=819513328';
+
+const telegramLink = 'https://t.me/+Mphy0wV4LYZkNTI1';
+const githubLink = 'https://github.com/FishHawk/auto-novel';
 </script>
 
 <template>
@@ -136,42 +145,27 @@ const linkExample = [
   </div>
 
   <div class="layout-content">
-    <n-space :wrap="false" align="center" style="margin-top: 8px">
-      <img v-if="isDesktop" :src="qqUrl" width="120" />
-
-      <n-ul>
-        <n-li>最近命犯太岁，服务器不是停电就是断网，各位活用下载功能吧。</n-li>
-        <n-li>
-          如果发现Sakura某段翻译得不准确，可以点击该段提交（需要登录），帮助我们改善Sakura模型。
-        </n-li>
-        <n-li>
-          <b>使用说明</b>
-          ：将想要翻译的小说链接复制到网站首页的输入框里，点击搜索，如果链接正确，将会跳转到小说页面。
-          <n-ul>
-            <n-li>
-              支持的小说站参考
-              <n-text type="success" @click="showLinkExampleModal = true">
-                小说链接示例
-              </n-text>
-              。
-            </n-li>
-            <n-li>
-              想自己生成翻译参考
-              <RouterNA to="/forum/64f3d63f794cbb1321145c07">插件教程</RouterNA>
-              。
-            </n-li>
-            <n-li>
-              有什么问题和建议请在
-              <RouterNA to="/forum/64f3e280794cbb1321145c09">
-                反馈&更新日志
-              </RouterNA>
-              集中讨论。
-            </n-li>
-          </n-ul>
-        </n-li>
-        <n-li>交流群：819513328，验证答案是“绿色”。</n-li>
-      </n-ul>
-    </n-space>
+    <n-ul style="margin-top: 24px">
+      <n-li>1月26日下午网站会暂停几个小时。</n-li>
+      <n-li>
+        如果发现Sakura某段翻译得不准确，可以点击该段提交（需要登录），帮助我们改善Sakura模型。
+      </n-li>
+      <n-li>
+        <n-flex>
+          <n-button text type="primary" @click="showHowToUseModal = true">
+            使用说明
+          </n-button>
+          /
+          <n-button text type="primary" @click="showQQModal = true">
+            QQ群
+          </n-button>
+          /
+          <n-a :href="telegramLink" target="_blank">Telegram</n-a>
+          /
+          <n-a :href="githubLink" target="_blank">Github</n-a>
+        </n-flex>
+      </n-li>
+    </n-ul>
 
     <template v-if="userData.isLoggedIn">
       <SectionHeader title="我的收藏">
@@ -194,15 +188,29 @@ const linkExample = [
     <n-divider />
   </div>
 
-  <card-modal title="链接示例" v-model:show="showLinkExampleModal">
-    <n-list>
-      <n-list-item v-for="[name, link] of linkExample">
-        <n-thing>
-          <b>{{ name }}</b>
-          <br />
-          {{ link }}
-        </n-thing>
-      </n-list-item>
-    </n-list>
+  <card-modal title="使用说明" v-model:show="showHowToUseModal">
+    <n-p>
+      将小说链接复制到网站首页的输入框里，点击搜索，如果链接正确，将会跳转到小说页面。更高级的用法，例如生成机翻、高级搜索等，参见
+      <RouterNA to="/forum/64f3d63f794cbb1321145c07">使用教程</RouterNA>
+      。有什么问题和建议请在
+      <RouterNA to="/forum/64f3e280794cbb1321145c09"> 反馈&建议 </RouterNA>
+      集中讨论。
+    </n-p>
+    <n-p> 支持的小说站如下: </n-p>
+    <n-p v-for="[name, link] of linkExample">
+      <b>{{ name }}</b>
+      <br />
+      <span v-html="link" />
+    </n-p>
+  </card-modal>
+
+  <card-modal title="QQ群" v-model:show="showQQModal">
+    <n-p>
+      交流群：
+      <n-a :href="qqLink" target="_blank">819513328</n-a>
+      ，验证答案是“绿色”。
+      <br />
+      <n-qr-code :size="150" :value="qqLink" />
+    </n-p>
   </card-modal>
 </template>
