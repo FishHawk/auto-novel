fd45516423734b972e9896ad1b9059112906ce91
FishHawk <whang177@gmail.com>
2024-02-08 21:26:18 +0800

    web: fix image card for some browser

--- web/src/components/ImageCard.vue
+++ web/src/components/ImageCard.vue
@@ -12,10 +12,15 @@ defineProps<{
     <img
       :src="src ? src : coverPlaceholder"
       alt="cover"
-      style="aspect-ratio: 1 / 1.5; object-fit: cover; border-radius: 2px"
+      style="
+        width: 100%;
+        aspect-ratio: 1 / 1.5;
+        object-fit: cover;
+        border-radius: 2px;
+      "
     />
 
-    <n-text style="height: 2.5em; padding: 8px" class="text-2line">
+    <n-text tag="div" style="height: 2.5em; padding: 8px" class="text-2line">
       {{ title }}
     </n-text>
   </n-flex>
