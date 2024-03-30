d75c15fc7e3f0cc620131a4cb10d254547d7ef30
FishHawk <whang177@gmail.com>
2024-03-31 03:49:09 +0800

    web: fix author search button

--- web/src/pages/novel/components/WebNovelMetadata.vue
+++ web/src/pages/novel/components/WebNovelMetadata.vue
@@ -36,6 +36,15 @@ const labels = computed(() => {
     .join(' / ');
   return labels;
 });
+
+const includesWhitespace = (s: string) => s.includes(' ') || s.includes('　');
+
+const generateSearchUrl = (query: string) => {
+  if (includesWhitespace(query)) {
+    query = `"${query}"`;
+  }
+  return `/novel-list?query=${encodeURIComponent(query)}`;
+};
 </script>
 
 <template>
@@ -73,22 +82,11 @@ const labels = computed(() => {
   <n-p>{{ labels }}</n-p>
 
   <n-p>
-    <c-a
-      :to="`/novel-list?page=1&query=${encodeURIComponent(
-        novel.titleJp
-      )}&selected=63&selected=0&selected=0&selected=0&selected=2`"
-    >
-      搜索标题
-    </c-a>
-    /
-    <c-a
-      v-if="novel.authors"
-      :to="`/novel-list?page=1&query=${encodeURIComponent(
-        novel.authors[0].name
-      )}&selected=63&selected=0&selected=0&selected=0&selected=2`"
-    >
-      搜索作者
-    </c-a>
+    <c-a :to="generateSearchUrl(novel.titleJp)"> 搜索标题 </c-a>
+    <template v-if="novel.authors">
+      /
+      <c-a :to="generateSearchUrl(novel.authors[0].name)"> 搜索作者 </c-a>
+    </template>
   </n-p>
 
   <n-p style="word-break: break-all">
