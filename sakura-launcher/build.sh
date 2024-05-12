685f6f03b36f61a2515cecc031da7e737750b5f9
FishHawk <whang177@gmail.com>
2024-05-12 22:38:09 +0800

    feat(web): update sakura launcher

--- sakura-launcher/build.sh
+++ sakura-launcher/build.sh
@@ -9,10 +9,10 @@ cd build
 
 declare -a arr=(
     "cudart-llama-bin-win-cu11.7.1-x64.zip"
-    "llama-$VERSION-bin-win-cublas-cu11.7.1-x64.zip"
+    "llama-$VERSION-bin-win-cuda-cu11.7.1-x64.zip"
     #
     "cudart-llama-bin-win-cu12.2.0-x64.zip"
-    "llama-$VERSION-bin-win-cublas-cu12.2.0-x64.zip"
+    "llama-$VERSION-bin-win-cuda-cu12.2.0-x64.zip"
     #
     "llama-$VERSION-bin-win-avx-x64.zip"
     "llama-$VERSION-bin-win-avx2-x64.zip"
@@ -36,8 +36,8 @@ pack () {
     zip -r $1.zip $1
 }
 
-pack "sakura-launcher-cublas11-$VERSION" 0 1
-pack "sakura-launcher-cublas12-$VERSION" 2 3
+pack "sakura-launcher-cuda11-$VERSION" 0 1
+pack "sakura-launcher-cuda12-$VERSION" 2 3
 pack "sakura-launcher-avx-$VERSION" 4
 pack "sakura-launcher-avx2-$VERSION" 5
 pack "sakura-launcher-avx512-$VERSION" 6
