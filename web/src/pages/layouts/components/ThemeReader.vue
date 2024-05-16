7fdfa24ec10004ff6d3c8b7b940ded1e8f0b98f3
IronKinoko <kinoko.main@gmail.com>
2024-05-16 21:04:10 +0800

    feat: support pwa

--- web/src/pages/layouts/components/ThemeReader.vue
+++ web/src/pages/layouts/components/ThemeReader.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
-import { darkTheme, dateZhCN, zhCN, useOsTheme } from 'naive-ui';
+import { lightTheme, darkTheme, dateZhCN, zhCN, useOsTheme } from 'naive-ui';
 
 import { Locator } from '@/data';
 import { isDarkColor } from '@/pages/util';
+import type { BuiltInGlobalTheme } from 'naive-ui/es/themes/interface';
 
 const osThemeRef = useOsTheme();
 
@@ -10,24 +11,37 @@ const { setting } = Locator.readerSettingRepository();
 const readerTheme = computed(() => {
   const readerTheme = setting.value.theme;
 
-  let theme: typeof darkTheme | null = null;
+  let theme: BuiltInGlobalTheme = lightTheme;
   let bodyColor: string | undefined = undefined;
 
   if (readerTheme.mode === 'light') {
-    theme = null;
+    theme = lightTheme;
   } else if (readerTheme.mode === 'dark') {
     theme = darkTheme;
   } else if (readerTheme.mode === 'system') {
     if (osThemeRef.value) {
-      theme = osThemeRef.value === 'dark' ? darkTheme : null;
+      theme = osThemeRef.value === 'dark' ? darkTheme : lightTheme;
     }
   } else if (readerTheme.mode === 'custom') {
-    theme = isDarkColor(readerTheme.bodyColor) ? darkTheme : null;
+    theme = isDarkColor(readerTheme.bodyColor) ? darkTheme : lightTheme;
     bodyColor = readerTheme.bodyColor;
   }
 
   return { theme, bodyColor: bodyColor ? { bodyColor } : undefined };
 });
+
+watch(
+  readerTheme,
+  (readerTheme) => {
+    const bodyColor =
+      readerTheme.bodyColor?.bodyColor ?? readerTheme.theme.common.bodyColor;
+    if (bodyColor) {
+      const meta = document.querySelector('meta[name="theme-color"]')!;
+      meta.setAttribute('content', bodyColor);
+    }
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
