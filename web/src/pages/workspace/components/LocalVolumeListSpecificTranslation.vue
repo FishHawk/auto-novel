a68c1a8eb15c5ea84681e4d90e61ba1cd15a6829
FishHawk <whang177@gmail.com>
2024-05-21 09:21:09 +0800

    make it easier

--- web/src/pages/workspace/components/LocalVolumeListSpecificTranslation.vue
+++ web/src/pages/workspace/components/LocalVolumeListSpecificTranslation.vue
@@ -13,7 +13,6 @@ import LocalVolumeList from './LocalVolumeList.vue';
 const props = defineProps<{ type: 'gpt' | 'sakura' }>();
 
 const message = useMessage();
-const router = useRouter();
 
 const { setting } = Locator.settingRepository();
 
@@ -121,13 +120,6 @@ const progressFilterFunc = computed(() => {
     return undefined;
   }
 });
-
-const readVolume = (volume: LocalVolumeMetadata): void => {
-  const url = router.resolve({
-    path: `/workspace/reader/${encodeURIComponent(volume.id)}/0`,
-  });
-  window.open(url.href, '_blank');
-};
 </script>
 
 <template>
@@ -173,13 +165,13 @@ const readVolume = (volume: LocalVolumeMetadata): void => {
             @action="queueVolume(volume.id)"
           />
 
-          <c-button
+          <router-link
             v-if="!volume.id.endsWith('.epub')"
-            label="阅读"
-            size="tiny"
-            @click="readVolume(volume)"
-            secondary
-          />
+            :to="`/workspace/reader/${encodeURIComponent(volume.id)}/0`"
+            target="_blank"
+          >
+            <c-button label="阅读" size="tiny" secondary />
+          </router-link>
 
           <c-button
             label="下载"
