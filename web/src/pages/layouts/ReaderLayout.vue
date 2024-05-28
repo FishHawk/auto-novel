878db9a3040796067cb9b8fbd6128c45dc3e9494
IronKinoko <32838658+IronKinoko@users.noreply.github.com>
2024-05-28 20:43:03 +0800

    fix(web): keep-alive component in nested route result in child route mounted twice (#64)

--- web/src/pages/layouts/ReaderLayout.vue
+++ web/src/pages/layouts/ReaderLayout.vue
@@ -1,5 +1,5 @@
 <template>
   <n-layout style="width: 100%; min-height: 100vh">
-    <router-view />
+    <router-view name="reader" />
   </n-layout>
 </template>
--- web/src/router.ts
+++ web/src/router.ts
@@ -42,11 +42,15 @@ const router = createRouter({
       children: [
         {
           path: '/novel/:providerId/:novelId/:chapterId',
-          component: () => import('./pages/reader/Reader.vue'),
+          components: {
+            reader: () => import('./pages/reader/Reader.vue'),
+          },
         },
         {
           path: '/workspace/reader/:novelId/:chapterId',
-          component: () => import('./pages/reader/Reader.vue'),
+          components: {
+            reader: () => import('./pages/reader/Reader.vue'),
+          },
         },
       ],
     },
