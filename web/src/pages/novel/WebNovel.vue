19d69336349cb259df7001d6edd154df35993fe0
涂俊宏 <tujunhong0014@gmail.com>
2024-10-17 13:54:16 +0800

    fix(web): Fix the style of the directory tag visited in web-novel-narrow

--- web/src/pages/novel/WebNovel.vue
+++ web/src/pages/novel/WebNovel.vue
@@ -27,22 +27,6 @@ store.loadNovel().then((result) => {
     document.title = result.value.titleJp;
   }
 });
-
-// TODO: 代码抽离
-const vars = useThemeVars();
-const mixColor = () => {
-  const color = vars.value.primaryColor;
-  const r = parseInt(color.substring(1, 3), 16);
-  const g = parseInt(color.substring(3, 5), 16);
-  const b = parseInt(color.substring(5, 7), 16);
-
-  const p = 0.5;
-  const mr = (r * p + 255 * (1 - p)).toFixed(0);
-  const mg = (g * p).toFixed(0);
-  const mb = (b * p).toFixed(0);
-  return `rgb(${mr}, ${mg}, ${mb})`;
-};
-const visitedColor = mixColor();
 </script>
 
 <template>
@@ -63,13 +47,3 @@ const visitedColor = mixColor();
     </c-result>
   </div>
 </template>
-
-<style scoped>
-:deep(.toc) {
-  cursor: default;
-}
-
-:deep(.toc:visited .toc-title-visited) {
-  color: v-bind('visitedColor');
-}
-</style>
--- web/src/pages/novel/components/WebNovelTocItem.vue
+++ web/src/pages/novel/components/WebNovelTocItem.vue
@@ -26,6 +26,22 @@ const type = computed(() => {
     }
   }
 });
+
+// TODO: 代码抽离
+const vars = useThemeVars();
+const mixColor = () => {
+  const color = vars.value.primaryColor;
+  const r = parseInt(color.substring(1, 3), 16);
+  const g = parseInt(color.substring(3, 5), 16);
+  const b = parseInt(color.substring(5, 7), 16);
+
+  const p = 0.5;
+  const mr = (r * p + 255 * (1 - p)).toFixed(0);
+  const mg = (g * p).toFixed(0);
+  const mb = (b * p).toFixed(0);
+  return `rgb(${mr}, ${mg}, ${mb})`;
+};
+const visitedColor = mixColor();
 </script>
 
 <template>
@@ -66,7 +82,13 @@ const type = computed(() => {
 </template>
 
 <style scoped>
+.toc {
+  cursor: default;
+}
 .toc:hover .toc-title {
   text-decoration: underline;
+}
+.toc:visited .toc-title-visited {
+  color: v-bind('visitedColor');
 }
 </style>
