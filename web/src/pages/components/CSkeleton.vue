b598e5d6f4998f3007451f93225d63281bc786d0
涂俊宏 <tujunhong0014@gmail.com>
2024-10-20 18:10:58 +0800

    feat(web): Add skeleton screen loading to homepage

--- web/src/pages/components/CSkeleton.vue
+++ web/src/pages/components/CSkeleton.vue
@@ -1,61 +1,80 @@
 <script lang="ts" setup>
-defineProps<{
-  type: 'webNovel' | 'wenkuNovel' | 'webNovelLite' | 'webNovelHistory';
-}>();
+withDefaults(
+  defineProps<{
+    type: 'webNovel' | 'wenkuNovel' | 'webNovelLite' | 'webNovelHistory';
+    length?: number;
+  }>(),
+  {
+    length: 20,
+  },
+);
+
 import { checkIsMobile } from '@/pages/util';
 const isMobile = checkIsMobile();
 </script>
 
 <template>
-  <template v-if="type === 'webNovel'">
-    <n-space size="small" vertical v-for="item in 20">
-      <template v-if="!isMobile">
-        <n-skeleton text :repeat="2" style="width: 60%" />
-        <n-skeleton text style="width: 25%" />
-        <n-skeleton text style="width: 60%" />
-        <n-skeleton text style="width: 40%" />
-        <n-skeleton text style="width: 15%" />
-      </template>
-      <template v-else>
-        <n-skeleton text :repeat="2" />
-        <n-skeleton text style="width: 80%" />
-        <n-skeleton text :repeat="2" />
-        <n-skeleton text style="width: 80%" />
-        <n-skeleton text style="width: 40%" />
-      </template>
-      <n-divider />
-    </n-space>
-  </template>
+  <div>
+    <template v-if="type === 'webNovel'">
+      <n-space
+        size="small"
+        vertical
+        v-for="(item, index) in length"
+        :key="index"
+      >
+        <template v-if="!isMobile">
+          <n-skeleton text :repeat="2" style="width: 60%" />
+          <n-skeleton text style="width: 25%" />
+          <n-skeleton text style="width: 60%" />
+          <n-skeleton text style="width: 40%" />
+          <n-skeleton text style="width: 15%" />
+        </template>
+        <template v-else>
+          <n-skeleton text :repeat="2" />
+          <n-skeleton text style="width: 80%" />
+          <n-skeleton text :repeat="2" />
+          <n-skeleton text style="width: 80%" />
+          <n-skeleton text style="width: 40%" />
+        </template>
+        <n-divider v-if="index < length - 1" />
+      </n-space>
+    </template>
 
-  <template v-if="type === 'webNovelLite' || type === 'webNovelHistory'">
-    <n-space size="small" vertical v-for="item in 20">
-      <template v-if="!isMobile">
-        <n-skeleton text :repeat="2" style="width: 60%" />
-        <n-skeleton text style="width: 40%" />
-        <n-skeleton text style="width: 15%" />
-        <n-skeleton :width="30" round v-if="type === 'webNovelHistory'" />
-      </template>
-      <template v-else>
-        <n-skeleton text :repeat="2" />
-        <n-skeleton text style="width: 80%" />
-        <n-skeleton text style="width: 40%" />
-        <n-skeleton :width="30" round v-if="type === 'webNovelHistory'" />
-      </template>
-      <n-divider />
-    </n-space>
-  </template>
+    <template v-if="type === 'webNovelLite' || type === 'webNovelHistory'">
+      <n-space
+        size="small"
+        vertical
+        v-for="(item, index) in length"
+        :key="index"
+      >
+        <template v-if="!isMobile">
+          <n-skeleton text :repeat="2" style="width: 60%" />
+          <n-skeleton text style="width: 40%" />
+          <n-skeleton text style="width: 15%" />
+          <n-skeleton :width="30" round v-if="type === 'webNovelHistory'" />
+        </template>
+        <template v-else>
+          <n-skeleton text :repeat="2" />
+          <n-skeleton text style="width: 80%" />
+          <n-skeleton text style="width: 40%" />
+          <n-skeleton :width="30" round v-if="type === 'webNovelHistory'" />
+        </template>
+        <n-divider v-if="index < length - 1" />
+      </n-space>
+    </template>
 
-  <template v-if="type === 'wenkuNovel'">
-    <n-grid :x-gap="12" :y-gap="12" cols="2 500:3 800:4">
-      <n-grid-item v-for="item in 20">
-        <div class="skeleton-cover">
-          <n-skeleton class="skeleton-img" />
-          <img class="skeleton-bg" src="@/image/girl.webp" alt="girl" />
-        </div>
-        <n-skeleton class="skeleton-title" text style="width: 80%" />
-      </n-grid-item>
-    </n-grid>
-  </template>
+    <template v-if="type === 'wenkuNovel'">
+      <n-grid :x-gap="12" :y-gap="12" cols="2 500:3 800:4">
+        <n-grid-item v-for="(item, index) in length" :key="index">
+          <div class="skeleton-cover">
+            <n-skeleton class="skeleton-img" />
+            <img class="skeleton-bg" src="@/image/girl.webp" alt="girl" />
+          </div>
+          <n-skeleton class="skeleton-title" text style="width: 80%" />
+        </n-grid-item>
+      </n-grid>
+    </template>
+  </div>
 </template>
 
 <style scoped>
