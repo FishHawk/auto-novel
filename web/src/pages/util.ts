6625225aca4ebfef9e294fa0222a0b97f7801aaa
TheNano <66905920+LittleSurvival@users.noreply.github.com>
2024-11-17 01:38:46 +0800

    Update util.ts

--- web/src/pages/util.ts
+++ web/src/pages/util.ts
@@ -19,7 +19,6 @@ export const useBreakPoints = () =>
   });
 
 export const checkIsMobile = () => {
-  return true;
   const a = navigator.userAgent || navigator.vendor || (window as any).opera;
   if (
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
