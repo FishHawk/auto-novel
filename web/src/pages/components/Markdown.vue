0fb2c5124453ddbe76495de6c988022053add259
IronKinoko <kinoko_main@outlook.com>
2024-11-22 11:04:57 +0800

    feat(web): 为Markdown组件中的链接添加target属性以在新标签页中打开

--- web/src/pages/components/Markdown.vue
+++ web/src/pages/components/Markdown.vue
@@ -21,6 +21,21 @@ md.linkify.add('http:', {
   },
 });
 
+// Remember the old renderer if overridden, or proxy to the default renderer.
+const defaultRender =
+  md.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  // Add a new `target` attribute, or replace the value of the existing one.
+  tokens[idx].attrSet('target', '_blank');
+
+  // Pass the token to the default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 const vars = useThemeVars();
 </script>
 
@@ -29,6 +44,10 @@ const vars = useThemeVars();
 </template>
 
 <style>
+.markdown {
+  overflow-wrap: break-word;
+  word-break: break-word;
+}
 .markdown a {
   transition: color 0.3s v-bind('vars.cubicBezierEaseInOut');
   cursor: pointer;
