d465b8908fd7070333a09e3af2a01d44fb007633
FishHawk <whang23@outlook.com>
2025-03-06 00:56:10 +0800

    fix(server): 修复syosetu解析器

--- server/src/main/kotlin/infra/web/datasource/providers/Syosetu.kt
+++ server/src/main/kotlin/infra/web/datasource/providers/Syosetu.kt
@@ -160,11 +160,11 @@ class Syosetu(
             .selectFirst("h1")!!
             .text()
 
-        val infodataEl = doc2.getElementById("infodata")!!
-        val preInfoEl = infodataEl.getElementById("pre_info")!!
+        val infodataEl = doc2.getElementsByClass("p-infotop-data").first()!!
+        val infotypeEl = doc2.getElementsByClass("p-infotop-type").first()!!
 
         fun row(label: String) = infodataEl
-            .selectFirst("th:containsOwn(${label})")
+            .selectFirst("dt:containsOwn(${label})")
             ?.nextElementSibling()
 
         val author = row("作者名")!!
@@ -175,10 +175,7 @@ class Syosetu(
                 )
             }
 
-        val type = (
-                preInfoEl.selectFirst("#noveltype_notend")
-                    ?: preInfoEl.selectFirst("#noveltype")!!
-                )
+        val type = infotypeEl.selectFirst(".p-infotop-type__type")!!
             .text()
             .let {
                 when (it) {
@@ -201,7 +198,7 @@ class Syosetu(
                     else -> keywords.add(it)
                 }
             }
-        preInfoEl
+        infotypeEl
             .selectFirst("#age_limit")
             ?.text()
             ?.let {
