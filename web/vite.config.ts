815e133879934a1ce851871c400b8c3e5ceeac91
FishHawk <whang23@outlook.com>
2025-05-01 01:48:55 +0800

    chore(web): 开发服务器现在支持下载文件

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,45 +1,85 @@
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite';
+import imagemin from 'unplugin-imagemin/vite';
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
-import { ProxyOptions, defineConfig, loadEnv } from 'vite';
+import { PluginOption, ServerOptions, defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import imagemin from 'unplugin-imagemin/vite';
+
+const defineServerOptions = (localServer: boolean): ServerOptions => {
+  return {
+    proxy: {
+      '/api': {
+        target: localServer
+          ? 'http://localhost:8081'
+          : 'https://books.fishhawk.top',
+        changeOrigin: true,
+        bypass: (req, _res, _options) => {
+          if (req.url && req.url.includes('/translate-v2/')) {
+            if (req.url.includes('/chapter/')) {
+              console.log('检测到小说章节翻译请求，已拦截');
+              return false;
+            }
+          }
+        },
+        rewrite: (path) => {
+          if (localServer) {
+            path = path.replace(/^\/api/, '');
+          }
+          return path;
+        },
+      },
+      '/files-temp': {
+        target: 'https://books.fishhawk.top',
+        changeOrigin: true,
+      },
+    },
+  };
+};
+
+const filesProxyPlugin = (): PluginOption => ({
+  name: 'files-proxy',
+  configureServer(server) {
+    server.middlewares.use('/files-temp', (req, res) => {
+      const url = new URL('http://localhost' + req.url);
+      const ext = path.extname(url.pathname).toLowerCase();
+      const mimeTypes = {
+        '.epub': 'application/epub+zip',
+        '.txt': 'text/plain',
+      };
+      res.setHeader(
+        'content-type',
+        mimeTypes[ext] || 'application/octet-stream',
+      );
+
+      const filePath = path.join(
+        __dirname,
+        '../server/data/files-temp',
+        url.pathname,
+      );
+      const content = fs.readFileSync(filePath);
+      res.end(content);
+    });
+  },
+});
 
 export default defineConfig(({ command, mode }) => {
-  const proxy: Record<string, ProxyOptions> = {};
+  let serverOptions: ServerOptions = {};
+  let pluginOptions: PluginOption[] = [];
 
   if (command === 'serve') {
     const env = loadEnv(mode, process.cwd(), 'LOCAL');
-    let proxyOptions: ProxyOptions;
-    if ('LOCAL' in env)
-      proxyOptions = {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      };
-    else
-      proxyOptions = {
-        target: 'https://books.fishhawk.top',
-        changeOrigin: true,
-      };
-    proxy['/api'] = {
-      ...proxyOptions,
-      bypass(req, _res, _options) {
-        if (req.url && req.url.includes('/translate-v2/')) {
-          if (req.url.includes('/chapter/')) {
-            console.log('检测到小说章节翻译请求，已拦截');
-            return false;
-          }
-        }
-      },
-    };
+    const localServer = 'LOCAL' in env;
+    serverOptions = defineServerOptions(localServer);
+    if (localServer) {
+      pluginOptions.push(filesProxyPlugin());
+    }
   }
   return {
-    server: {
-      proxy,
-    },
+    server: serverOptions,
     build: {
       target: ['es2015', 'edge88', 'firefox78', 'chrome87', 'safari14'],
       cssCodeSplit: false,
@@ -62,6 +102,7 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     plugins: [
+      ...pluginOptions,
       vue(),
       imagemin(),
       createHtmlPlugin({
