1ffc8bcf24784207a4e1d74834ad8e0181facbd5
FishHawk <whang23@outlook.com>
2025-05-18 12:52:05 +0800

    chore(server): 增加es数据库初始化

--- server/src/main/kotlin/infra/ElasticSearchClient.kt
+++ server/src/main/kotlin/infra/ElasticSearchClient.kt
@@ -3,6 +3,8 @@ package infra
 import com.jillesvangurp.ktsearch.KtorRestClient
 import com.jillesvangurp.ktsearch.Node
 import com.jillesvangurp.ktsearch.SearchClient
+import com.jillesvangurp.ktsearch.createIndex
+import kotlinx.coroutines.runBlocking
 
 typealias ElasticSearchClient = SearchClient
 
@@ -11,7 +13,45 @@ fun elasticSearchClient(host: String, port: Int?) =
         KtorRestClient(
             Node(host, port ?: 9200),
         )
-    )
+    ).apply {
+        runBlocking {
+            runCatching {
+                createIndex(ElasticSearchIndexNames.WEB_NOVEL) {
+                    mappings(dynamicEnabled = false) {
+                        keyword("providerId")
+                        text("titleJp") { analyzer = "icu_analyzer" }
+                        text("titleZh") { analyzer = "icu_analyzer" }
+                        keyword("authors")
+                        keyword("type")
+                        keyword("attentions")
+                        keyword("keywords")
+                        number<Int>("tocSize")
+                        number<Int>("visited")
+                        bool("hasGpt")
+                        bool("hasSakura")
+                        date("updateAt")
+                    }
+                }
+            }
+
+            runCatching {
+                createIndex(ElasticSearchIndexNames.WENKU_NOVEL) {
+                    mappings(dynamicEnabled = false) {
+                        text("title") { analyzer = "icu_analyzer" }
+                        text("titleZh") { analyzer = "icu_analyzer" }
+                        keyword("authors")
+                        keyword("artists")
+                        keyword("keywords")
+                        keyword("level")
+                        keyword("publisher")
+                        keyword("imprint")
+                        date("latestPublishAt")
+                        date("updateAt")
+                    }
+                }
+            }
+        }
+    }
 
 object ElasticSearchIndexNames {
     const val WEB_NOVEL = "web.2024-06-10"
